af5eb3ec58d0bbc89cecc876549fd8c1a4d1fb12
beomki69 <beomki69@gmail.com>
2021-08-20 16:28:18 +0900

    rsp-function

--- rsp/client.jsx
+++ rsp/client.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import RSPClass from "./RSPClass";
+import RSPFunction from "./RSPFunction";
 
 ReactDOM.render(
   <>
     <RSPClass />
+    <RSPFunction />
   </>,
   document.querySelector("#root")
 );
